81a214fb7166fea11132e0b7ccbac074de29104b
adorahelen <adorahelenmin@gmail.com>
2024-09-18 14:55:29 +0900

    .

---
+++ src/main/java/kdt/hackathon/practicecommunity/PracticeCommunityApplication.java
@@ -0,0 +1,13 @@
+package kdt.hackathon.practicecommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeCommunityApplication.class, args);
+    }
+
+}
