d8a0d6879bc52f48bf120bb056ce2aa47df7be07
adorahelen <adorahelenmin@gmail.com>
2024-09-22 23:42:54 +0900

    10. 댓글 조회 테스트(유저로)

--- src/test/java/kdt/hackathon/practicecommunity/TestRepository/CommentRepositoryTest.java
+++ src/test/java/kdt/hackathon/practicecommunity/TestRepository/CommentRepositoryTest.java
@@ -70,8 +70,22 @@ public class CommentRepositoryTest {
 
         assertThat(comments).isNotNull();
         assertThat(comments.size()).isEqualTo(2); // 생성되어 있는 댓글은 2개
-
-
     }
+
+    @Test
+    @Transactional
+    @DisplayName("findByUser")
+    public void findByUserTest() {
+        User user = userRepository.findByEmail("test@example.com").orElseThrow();
+        List<Comment> comments = commentRepository.findByUser(user);
+        comments.forEach(comment -> log.info(comment.toString()));
+        assertThat(comments).isNotNull();
+        assertThat(comments.size()).isEqualTo(2);
+        // 이메일을 통해 유저를 알아내고, 유저를 바탕으로 작성한 모든 댓글을 찾아 조회한다
+        // * 추가 할 사항으로는 댓글을 1번 피드 뿐만 아니라 2번, 3번 피드에도 작성하고 (유저 다르게)
+            // 개별적인 피드에 무분별하게 달려있는 댓글들이 한번에 조회되는지를 테스트 하길 바람
+    }
+
+    // 책에서는 네이티브 쿼리 메서드 (@Query, orm.xml) 을 사용했지만, 여기서는 사용 X -> 자동 생성
 
 }
